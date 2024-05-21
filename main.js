3c87de711574568979776fe62948ce313f278f6a
harrylever <ukanah15thdean@gmail.com>
2024-05-21 17:00:09 +0100

    feat: finished touches

--- main.js
+++ main.js
@@ -2,10 +2,25 @@ let realForm = document.querySelector('form');
 let emailVal = document.querySelector('#email');
 let passwordVal = document.querySelector('#password');
 
-realForm.addEventListener('submit', (e) => {
+async function postLogin(email, password) {
+    const response = await fetch("https://mira-backend.azurewebsites.net/api/auth/login", {
+      method: "POST",
+      body: { email, password },
+    });
+
+    return response;
+}
+
+realForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const email = emailVal.value;
     const password = passwordVal.value;
-    console.log({ email, password });
+    
+    try {
+        const res = await postLogin(email, password);
+        console.log(res);
+    } catch (err) {
+        console.log(err);
+    }
 })
